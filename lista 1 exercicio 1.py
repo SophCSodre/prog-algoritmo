3826273403f6e54a880507aed5182fc20db76127
João Pedro <joaopedrowatson@hotmail.com>
2022-02-19 15:07:54 -0300

    Arrumando exercicio 6 da lista 1

--- lista 1 exercicio 1.py
+++ lista 1 exercicio 1.py
@@ -1,5 +1,5 @@
 x = int (input ("digite o valor:"))
-y = x - 3
+y = x + 3
 z = y * 2
 print("o resultado é: " , z)
                 
--- lista 1 exercicio 6.py
+++ lista 1 exercicio 6.py
@@ -1,6 +1,6 @@
-x = int (input ("Digite o valor1: "))
+x = int(input("Digite o valor1: "))
 y = x - 3
-w = int (input ("Digite o valor2: "))
+w = int(input("Digite o valor2: "))
 z = w + 2
 a = y + z
 b = a - 1
